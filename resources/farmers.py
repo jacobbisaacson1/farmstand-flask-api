4ee46cb67eeb8530bc5e8cdbd921b244116cb240
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-30 11:20:53 -0500

    farmer user added and printing in json in terminal

--- resources/farmers.py
+++ resources/farmers.py
@@ -1,8 +1,20 @@
 import models
-from flask import Blueprint
+from flask import Blueprint, request
 
 farmers = Blueprint('farmers', 'farmers')
 
 @farmers.route('/', methods=['GET'])
 def test_farmer_resource():
 	return "farmer resource set up!"
+
+
+@farmers.route('/register', methods=['POST'])
+def register():
+  print(request.get_json())
+  return "check terminal for register json stuff"
+
+
+
+
+
+  
