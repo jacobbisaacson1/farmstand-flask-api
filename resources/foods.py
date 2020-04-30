fb05b35183a11518a30754ea321272f3da36ba56
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-30 10:39:59 -0500

    apple added to DB

--- resources/foods.py
+++ resources/foods.py
@@ -3,12 +3,21 @@ from flask import Blueprint, request
 
 foods = Blueprint('foods', 'foods')
 
+# GET /api/v1/foods
+# SHOW / INDESX
 @foods.route('/', methods=['GET'])
 def foods_index():
 	return "foods resource working"
 
+# POST /api/v1/foods/ (dont forget the slash)
+# CREATE
 @foods.route('/', methods=['POST'])
 def create_food():
   payload = request.get_json()
   print(payload)
-  return "check terminal"
+  new_food = models.Food.create(
+    name=payload['name'],
+    price=payload['price'],
+    farmer=payload['farmer']
+  )
+  return "check terminal -- should be actually creating food"
