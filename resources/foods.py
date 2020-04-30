f430d6de9337a6847eac6823b03b7bbff6a63f86
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-30 10:33:06 -0500

    get and post routes set up, need to user set up now per RAs note in slack

--- resources/foods.py
+++ resources/foods.py
@@ -1,8 +1,14 @@
 import models
-from flask import Blueprint
+from flask import Blueprint, request
 
 foods = Blueprint('foods', 'foods')
 
-@foods.route('/')
+@foods.route('/', methods=['GET'])
 def foods_index():
 	return "foods resource working"
+
+@foods.route('/', methods=['POST'])
+def create_food():
+  payload = request.get_json()
+  print(payload)
+  return "check terminal"
